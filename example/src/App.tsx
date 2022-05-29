33f130ccd73e40336cbef72b707ced519ef938df
Mukesh Jangid <jangid.mukesh990@gmail.com>
2022-05-29 15:58:44 +0530

    Initial module setup

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import { Counter } from 'MJRNModule'
+
+const App = () => {
+  useEffect(() => {
+    console.log('')
+  })
+
+  return <Counter />
+}
+
+export default App
